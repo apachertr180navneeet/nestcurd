d35a315bbe5fd8fb2ea48be1769a45e6a0ee88dc
apachertr180navneeet <navneetgehlot03061993@gmail.com>
2024-12-20 11:01:09 +0530

    basic setup for curd

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,4 +1,25 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { Injectable } from '@nestjs/common';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true, // Disable in production
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
 
 @Injectable()
 export class AppService {
@@ -6,3 +27,5 @@ export class AppService {
     return 'Hello World!';
   }
 }
+
+export class AppModule {}
